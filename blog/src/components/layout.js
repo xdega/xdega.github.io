f2b3896f79b7120455d5c3d38bdf2332297dea31
Liam Hockley <xdega121@gmail.com>
2021-06-16 15:46:58 -0700

    Update header

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -6,31 +6,38 @@ const Layout = ({ location, title, children }) => {
   const isRootPath = location.pathname === rootPath
   let header
 
-  if (isRootPath) {
-    header = (
-      <h1 className="text-5xl font-bold bg-blue-300 text-white px-2 py-2 inline">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
+  const headerCSS = [
+    "font-bold",
+    "bg-blue-300",
+    "text-white",
+    "px-2",
+    "py-2",
+    "pr-3",
+    "inline",
+    "bg-gradient-to-r",
+    "from-green-300",
+    "to-blue-400"
+  ];
+
+  // Make the heading smaller for non home page links
+  (isRootPath) ? headerCSS.push('text-5xl') :  headerCSS.push('text-3xl');
+
+
+  header = (
+    <h1 className={headerCSS.join(' ')}>
+      <Link to="/">{title}</Link>
+    </h1>
+  );
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        © {new Date().getFullYear()} | Liam Hockley
       </footer>
     </div>
-  )
+  );
 }
 
 export default Layout
