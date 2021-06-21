9b1ef9a5c351ecc8842efa45bb24c3de6e623854
Liam Hockley <xdega121@gmail.com>
2021-06-20 19:42:27 -0700

    Add initial Jest testing setup

---
+++ src/components/__tests__/bio.js
@@ -0,0 +1,12 @@
+import React from "react"
+import renderer from "react-test-renderer"
+import Header from "../bio"
+describe("Bio", () => {
+  
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<Header/>)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
