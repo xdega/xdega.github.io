bf09dead071de7cb134cbf42e705d428fb705a74
Liam Hockley <xdega121@gmail.com>
2021-06-25 19:42:53 -0700

    Disable warning for image loading.

--- src/components/__tests__/bio.js
+++ src/components/__tests__/bio.js
@@ -2,9 +2,28 @@ import React from "react"
 import { render } from "@testing-library/react"
 import Bio from "../bio"
 
-test("bio renders intro text correctly", () => {
-  const { getByTestId } = render(<Bio />);
+describe("<Bio /> component", () => {
+  const originalWarn = console.warn.bind(console.warn);
 
-  expect(getByTestId("bio-intro"))
-    .toHaveTextContent("I am a Software Engineer focused on Ed Tech and the WWW.")
-})
+  beforeAll(() => {
+    console.warn = (msg) => {
+      /**
+       * Disabling the image loading warning. Unable to find good documentation 
+       * on how to propery mock a component that contains images, but these 
+       * tests do not care about images per se.
+       * */
+      !msg.toString().includes('Image not loaded') && originalWarn(msg);
+    }
+  });
+
+  afterAll(() => {
+    console.warn = originalWarn
+  });
+
+  it("should render intro text correctly", () => {
+    const { getByTestId } = render(<Bio />);
+  
+    expect(getByTestId("bio-intro"))
+      .toHaveTextContent("I am a Software Engineer focused on Ed Tech and the WWW.")
+  })
+});
